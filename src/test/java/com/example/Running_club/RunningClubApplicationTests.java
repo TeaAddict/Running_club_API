4a0e846880f1a1c78f4044fce8bb8d81a37d537e
DeividasM <deividasmor7@gmail.com>
2025-02-17 07:21:05 +0200

    init commit

---
+++ src/test/java/com/example/Running_club/RunningClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Running_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
